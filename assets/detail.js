2501f609b9c717690b1dbcd7d3203a4757ad00f2
naecms <141725038+naecms@users.noreply.github.com>
2025-08-05 20:50:58 +0700

    Update detail.js

--- assets/detail.js
+++ assets/detail.js
@@ -19,10 +19,11 @@ fetch('assets/data.json')
       <img src="${file.img}" alt="${file.judul}" width="300">
       <h1>${file.judul}</h1>
       <p><strong>Genre :</strong> ${file.size}</p>
-      <p><a onclick="a1398()" class="btn_download" href="${file.link_download}" target="_blank">Download Now!</a></p>
+      <p><a onclick="a1398()" class="btn_download" href="${file.link_download}">Download Now!</a></p>
     `;
   })
   .catch(error => {
     console.error("Gagal memuat data:", error);
     document.getElementById("detail").innerHTML = "<p>Terjadi kesalahan saat memuat data.</p>";
   });
+
